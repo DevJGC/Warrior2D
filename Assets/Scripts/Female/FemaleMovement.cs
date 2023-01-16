7ad5ccf267683ae04d67fcd4f5500eeb8ee4517f
Jose Garcia <jgcordoba@gmail.com>
2023-01-16 16:03:11 +0100

    DialogEngine

--- Assets/Scripts/Female/FemaleMovement.cs
+++ Assets/Scripts/Female/FemaleMovement.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class FemaleMovement : MonoBehaviour
 {
@@ -14,14 +15,29 @@ public class FemaleMovement : MonoBehaviour
 
     float xFemale;
 
+    // Dialog panel
+    [SerializeField] GameObject dialogPanel;
+    // UI TMPro text
+    [SerializeField] TMP_Text dialogText;
+
+    // Counter flow dialog 
+    int dialogCounter;
+
     void Start()
     {
+        Invoke("ActiveDialogPanel",2);
+        // invoke text program sequence
+        StartCoroutine("DialogText","Cariño, la chiquilla no está en casa, ha dejado una nota...");   
+        // startcoroutine wait seconds
+        StartCoroutine("WaitSeconds",8);
         
+
     }
 
     void Update()
     {
         PlayerDistance();
+
     }
 
     // player distance
@@ -72,4 +88,68 @@ public class FemaleMovement : MonoBehaviour
         }
     }
 
+    // Active dialog panel
+    public void ActiveDialogPanel()
+    {
+        dialogPanel.SetActive(true);
+        dialogText.text = "";
+
+    }
+
+    // Close dialog panel
+    public void CloseDialogPanel()
+    {
+        dialogPanel.SetActive(false);
+       
+    }
+
+    // ienumerator Show Text
+    IEnumerator DialogText(string textDialog)
+    {
+        yield return new WaitForSeconds(2f);
+
+        for (int i = 0; i < textDialog.Length; i++)
+        {
+            dialogText.text += textDialog[i];
+            yield return new WaitForSeconds(0.05f);
+        }
+    }
+
+    // ienumerator wait seconds
+    IEnumerator WaitSeconds(float seconds)
+    {
+        yield return new WaitForSeconds(seconds);
+        //CloseDialogPanel();
+        
+    }
+
+    // function count dialog press button canvas
+
+    public void CountDialog()
+    {
+        dialogCounter++;
+        dialogText.text = "";
+        if (dialogCounter == 1)
+        {
+            StartCoroutine("DialogText","Dice que se quiere casar con el Jonathan, el hijo de la Paqui...");
+            StartCoroutine("WaitSeconds", 1);
+        }
+        if (dialogCounter == 2)
+        {
+            StartCoroutine("DialogText","Esa familia son todos unos perros. No dan palo al agua!!!...");
+            StartCoroutine("WaitSeconds", 1);
+        }
+        if (dialogCounter == 3)
+        {
+            StartCoroutine("DialogText","Así que ve a por ella antes de que me la preñen. VE!!!");
+            StartCoroutine("WaitSeconds", 1);
+        }
+        if (dialogCounter == 4)
+        {
+            CloseDialogPanel();
+        }
+    }
+
+
+    
 }
